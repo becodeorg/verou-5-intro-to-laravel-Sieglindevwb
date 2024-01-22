fc8433cb055e77e6d8dc85e28263ba2c3a221b9c
Sieglinde <sieglindevwb@gmail.com>
2024-01-22 16:45:52 +0100

    Add content and footer.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
 
 @include('header')
 
-@section('content')
-    <!-- Your specific page content goes here -->
-@endsection
-
+    <article class="container mt-4">
+        <h1>Welcome to My Website</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, quia eaque quae dolor magni magnam in optio maxime nostrum libero, neque unde non cupiditate accusamus itaque commodi, possimus quidem ut.</p>
+    </article>
 
 @include('footer')
 
