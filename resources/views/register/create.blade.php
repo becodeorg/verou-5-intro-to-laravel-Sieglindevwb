5a1570a48fa937ddcd7dadccb9747504e2bb4bcd
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-01-23 11:59:31 +0100

    Add style to form.

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -3,54 +3,55 @@
     <section class="container mt-5 mb-5">
         <main class="bg-light p-4 rounded border" >
             
-            <h1 class="d-flex justify-content-center">Register!</h1>
+            <h1 class="mb-4">Give input</h1>
 
-            <form action="POST" action="/register" class="max-w-lg" >
+            <form method="POST" action="/register">
+            <div class="form-row">
 
-                <div class="d-flex justify-content-center mt-4">
-                    <label class="form-label mr-2" 
-                    for="name">
-                    Name
-                </label>
-
-                <input class="form-control-sm" 
-                        type="text"
-                        name="name"
-                        id="name"
-                        required
-                    >
+                <div class="col-md-4 mb-3">
+                <label for="validationDefault01">First name</label>
+                <input type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
                 </div>
 
-                <div class="d-flex justify-content-center mt-4">
-                    <label class="form-label mr-2" 
-                    for="username">
-                    Username
-                </label>
-
-                <input class="form-control-sm" 
-                        type="text"
-                        name="username"
-                        id="username"
-                        required
-                    >
+                <div class="col-md-4 mb-3">
+                <label for="validationDefault02">Last name</label>
+                <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required>
                 </div>
 
-                <div class="d-flex justify-content-center mt-4">
-                    <label class="form-label mr-2" 
-                    for="password">
-                    Password
-                </label>
-
-                <input class="form-control-sm" 
-                        type="password"
-                        name="password"
-                        id="password"
-                        required
-                    >
+                <div class="col-md-4 mb-3">
+                <label for="validationDefaultUsername">Username</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroupPrepend2">@</span>
+                    </div>
+                    <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
                 </div>
+                </div>
+            </div>
 
-                <button type="submit" class="d-flex justify-content-center mt-4 btn btn-primary">Submit</button>
-                
+             <div class="form-group">
+                <label for="exampleFormControlInput1">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Subject/Title</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                <option>Favorite post</option>
+                <option>Update post</option>
+                <option>Review post</option>
+                <option>Delete post</option>
+                <option>Other</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Extra info</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">Type here...</textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
             </form>
         </main>
     </section>
